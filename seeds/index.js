772ea4c455345afa0031d92d0364f009844da6c1
sunchaser <anika.mclean@gmail.com>
2022-06-02 12:05:59 -0400

    updated clientData & index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,15 @@
 const seedDoctors = require("./doctorData");
+const seedClients = require("./clientData");
 const sequelize = require("../config/connection");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   await seedDoctors();
   console.log("--------------");
+
+  await seedClients();
+  console.log("--------------");
+
   process.exit(0);
 };
 
