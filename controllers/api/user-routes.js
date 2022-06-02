54becd1a7b83c901d739b0eb64e16ca1c5a2bff3
Luyan Zhang <z709973733@gmail.com>
2022-06-02 13:05:58 -0400

    added more api codes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -35,32 +35,72 @@ router.post("/", (req, res) => {
     email: req.body.email,
     password: req.body.password,
   })
-    .then((dbPostData) => res.json(dbPostData))
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-router.put("/:id", (req, res) => {
-  User.update(
-    {
-      username: req.body.username,
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: {
       email: req.body.email,
-      password: req.body.password,
     },
-    {
-      where: {
-        id: req.params.id,
-      },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
     }
-  )
-    .then((dbPostData) => {
-      if (!dbPostData) {
-        res.status(404).json({ message: "No User found with this id" });
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  });
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.put("/:id", (req, res) => {
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
         return;
       }
-      res.json(dbPostData);
+      res.json(dbUserData);
     })
     .catch((err) => {
       console.log(err);
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,12 @@ const sequelize = require("../config/connection");
 const { Clients, User, Doctors } = require("../models");
 
 router.get("/", (req, res) => {
-  Doctors.findAll()
+  Doctors.findAll({
+    include: {
+      model: Clients,
+      attributes: ["name"],
+    },
+  })
     .then((dbPostData) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
       res.render("main", {
@@ -14,6 +19,42 @@ router.get("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get("/doctor/:id", (req, res) => {
+  Doctors.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: {
+      model: Clients,
+      attributes: ["name"],
+    },
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No doctor found with this id" });
+        return;
+      }
+
+      const doctor = dbPostData.get({ plain: true });
+
+      res.render("single-doctor", {
+        doctor,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/sign-up", (req, res) => {
+  res.render("sign-up");
 });
 
 module.exports = router;
