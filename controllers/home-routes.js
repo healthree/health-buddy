114968ef11b374bdf8aa3d050cb41413572936fc
Luyan Zhang <z709973733@gmail.com>
2022-06-02 12:31:52 -0400

    main page html css

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,4 +2,18 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Clients, User, Doctors } = require("../models");
 
+router.get("/", (req, res) => {
+  Doctors.findAll()
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("main", {
+        posts,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
