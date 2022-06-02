5e956b4245ed3f8f70a3f91c028ce4363e62e524
Luyan Zhang <z709973733@gmail.com>
2022-06-02 12:44:14 -0400

    add more client, foctor and user api codes

--- controllers/api/client-routes.js
+++ controllers/api/client-routes.js
@@ -29,6 +29,50 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  Clients.create({
+    name: req.body.name,
+    date_of_birth: req.body.date_of_birth,
+    date_of_enrollment: req.body.date_of_enrollment,
+    symptoms: req.body.symptoms,
+    medical_history: req.body.medical_history,
+    doctors_id: req.body.doctors_id,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  Clients.update(
+    {
+      name: req.body.name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_enrollment: req.body.date_of_enrollment,
+      symptoms: req.body.symptoms,
+      medical_history: req.body.medical_history,
+      doctors_id: req.body.doctors_id,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No client found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 router.delete("/:id", (req, res) => {
   Clients.destroy({
@@ -38,7 +82,7 @@ router.delete("/:id", (req, res) => {
   })
     .then((dbPostData) => {
       if (!dbPostData) {
-        res.status(404).json({ message: "No doctor found with this id" });
+        res.status(404).json({ message: "No client found with this id" });
         return;
       }
       res.json(dbPostData);
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -29,6 +29,45 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  User.update(
+    {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No User found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 router.delete("/:id", (req, res) => {
   User.destroy({
     where: {
@@ -37,7 +76,7 @@ router.delete("/:id", (req, res) => {
   })
     .then((dbPostData) => {
       if (!dbPostData) {
-        res.status(404).json({ message: "No doctor found with this id" });
+        res.status(404).json({ message: "No user found with this id" });
         return;
       }
       res.json(dbPostData);
